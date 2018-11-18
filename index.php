3f653f714001e01d243127681ba4b861c5a68fc4
svnskpopova <36789650+svnskpopova@users.noreply.github.com>
2018-11-18 19:15:25 +0700

    работа с условиями
    добавлены фигурные скобки в коде.

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1) echo "checked"; ?>>
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1) {echo "checked";} ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
